e036c440faf16ec3037cfababaff65734598d82f
Murilo Pereira Grinaldi <160647039+Grinaldi20@users.noreply.github.com>
2025-07-14 12:04:07 -0300

    front

--- src/app/Carrinho/page.jsx
+++ src/app/Carrinho/page.jsx
@@ -1,8 +1,74 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+
+
 export default function Carrinho() {
     return (
-     <div>
-        <h1>Carrinho</h1>
+     <main className={styles.main}>
+  <div className={styles.container}>
+    <div className={styles.header}>
+      <a href="../TelaP.html">
+      <span className={styles.backArrow}>&#8592;</span>
+      </a>
+     <h1 className={styles.h1}>Reservados</h1>
+    </div>
+
+    <div className={styles.item}>
+       <Image 
+                src="/bolsa.png"
+                 width={90}
+                 height={90}  
+                 alt="Bolsa termica vermelha"
+                 quality={100}
+      />
+      
+      <div className={styles.itemInfo}>
+        <div className={styles.itemTitle}>Bolsa térmica vermelha</div>
+        <div className={styles.itemText}>Encontrada dia: 09/08/2024</div>
+        <div className={styles.itemText}>Local: Refeitório</div>
+        <div className={styles.itemText}>Classificação: Objetos gerais</div>
+        <button className={styles.reservdBtn}>Reservado</button>
       </div>
+    </div>
+    
+    <div className={styles.item}>
+      <Image 
+                src="/fone.png"
+                 width={90}
+                 height={90}  
+                 alt="Fone de ouvido"
+                 quality={100}
+      />
+      <div className={styles.itemInfo}>
+        <div className={styles.itemTitle}>Fone de ouvido Iphone branco</div>
+        <div className={styles.itemText}>Encontrada dia: 09/08/2024</div>
+        <div className={styles.itemText}>Local: Refeitório</div>
+        <div className={styles.itemText}>Classificação: Objetos gerais</div>
+        <button className={styles.reservdBtn}>Reservado</button>
+      </div>
+    </div>
+
+
+    <div className={styles.item}>
+        <Image 
+                src="/caderno.png"
+                 width={90}
+                 height={90}  
+                 alt="Caderno rosa"
+                 quality={100}
+      />
+      <div className={styles.itemInfo}>
+        <div className={styles.itemTitle}>Caderno rosa neon da Tilibra</div>
+        <div className={styles.itemText}>Encontrada dia: 09/08/2024</div>
+        <div className={styles.itemText}>Local: Refeitório</div>
+        <div className={styles.itemText}>Classificação: Objetos gerais</div>
+        <button className={styles.reservdBtn}>Reservado</button>
+      </div>
+    </div>
+
+    <div className={styles.footer}></div>
+  </div>
+</main>
     );
   }
