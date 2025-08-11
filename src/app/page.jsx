cf7b6560f8373b0263ac851071137ea590cc9201
Thiago Sampaio <berlim0608@gmail.com>
2025-08-11 14:43:43 -0300

    Responsividade
    O Carrocel ta indo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -35,33 +35,58 @@ export default function TelaPrincipal() {
       <main>
      
         <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
 
-          <div className="carousel-inner" style={{ height: "400px" }}>
-            <div className="carousel-item active">
-              <Image className={`d-block w-100 ${styles.imagem}`} src="/carrosel.png" alt="Slide 1" width={1200} height={400} quality={100} />
-            </div>
-            <div className="carousel-item">
-              <Image className={`d-block w-100 ${styles.imagem}`} src="/carrosel6.png" alt="Slide 2" width={1200} height={400} quality={100} />
-            </div>
-            <div className="carousel-item">
-              <Image className={`d-block w-100 ${styles.imagem}`} src="/carrosel2.png" alt="Slide 3" width={1200} height={400} quality={100} />
-            </div>
-          </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <Image
+        className={`d-block w-100 ${styles.imagem}`}
+        src="/carrosel.png"
+        alt="Slide 1"
+        width={0}
+        height={0}
+        sizes="100vw"
+        quality={100}
+      />
+    </div>
+    <div className="carousel-item">
+      <Image
+        className={`d-block w-100 ${styles.imagem}`}
+        src="/carrosel6.png"
+        alt="Slide 2"
+        width={0}
+        height={0}
+        sizes="100vw"
+        quality={100}
+      />
+    </div>
+    <div className="carousel-item">
+      <Image
+        className={`d-block w-100 ${styles.imagem}`}
+        src="/carrosel2.png"
+        alt="Slide 3"
+        width={0}
+        height={0}
+        sizes="100vw"
+        quality={100}
+      />
+    </div>
+  </div>
 
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span className={`carousel-control-prev-icon ${styles.carroselControlPrevIcon}`} aria-hidden="true"></span>
-            <span className="visually-hidden">Anterior</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span className={`carousel-control-next-icon ${styles.carroselControlNextIcon}`} aria-hidden="true"></span>
-            <span className="visually-hidden">Próximo</span>
-          </button>
-        </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className={`carousel-control-prev-icon ${styles.carroselControlPrevIcon}`} aria-hidden="true"></span>
+    <span className="visually-hidden">Anterior</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className={`carousel-control-next-icon ${styles.carroselControlNextIcon}`} aria-hidden="true"></span>
+    <span className="visually-hidden">Próximo</span>
+  </button>
+</div>
+
        
 
         <div className={styles.Cards}>
