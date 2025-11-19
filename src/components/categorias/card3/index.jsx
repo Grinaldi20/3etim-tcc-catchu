9ece27b4843eef0ee0527611d56b60913221b7fe
Murilo Pereira Grinaldi <160647039+Grinaldi20@users.noreply.github.com>
2025-11-19 08:12:39 -0300

    sobre

--- src/components/categorias/card3/index.jsx
+++ src/components/categorias/card3/index.jsx
@@ -3,27 +3,20 @@ import styles from './index.module.css';
 
 export default function CardSobre({ desc, onClick }) {
     return (
-     <div className={styles.teamGrid}>
- <div className={styles.teamCard}>
-          <Image 
+      <div className={styles.teamCard}>
+        <Image 
           src={desc.desc_foto} 
-          alt="Murilo Grinaldi"
-          width={150}
-          height={150}
-          
+          alt={desc.desc_nome || 'Foto'}
+          width={300}
+          height={220}
           />
-          <div className={styles.teamCardContent}>
-            <h3 className={styles.teamName}>{desc.desc_nome}</h3>
-            <p className={styles.teamRole}>{desc.desc_espec}</p>
-            <p className={styles.teamDesc}>
-             {desc.desc_func}
-            </p>
-
-            <button className={styles.button} onClick={() => onClick(desc)}>Ver</button>
-          </div>
+        <div className={styles.teamCardContent}>
+          <h3 className={styles.teamName}>{desc.desc_nome}</h3>
+          <p className={styles.teamRole}>{desc.desc_espec}</p>
+          <p className={styles.teamDesc}>{desc.desc_func}</p>
+          <button className={styles.button} onClick={() => onClick(desc)}>Ver</button>
         </div>
-</div>
-
+      </div>
     );
 }
 
